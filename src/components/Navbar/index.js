b6f8f7d1c4651a0cbb1ff74b0ea03e9a1ef4c3f0
saikumar <gattusaikumar369@gmail.com>
2024-09-19 11:12:26 +0530

    header

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,14 +17,14 @@ const Navbar = () => {
         <img src="https://workisy.com/uploaded-images/logo/pnyd1i5u2KOpKGk5XcajI6MShDnWHVkzU9PND19N.png" alt="Workisy Logo" className='nav-logo'/>
       </div>
       <ul className="nav-links-list">
-        <li><a href="https://workisy-postjob.netlify.app"  className='nav-list-item'>Post a job </a></li>
+        <li className='nav-list-item'>Post a job</li>
         <li onClick={toggleDatabase} className="nav-list-item">
           Candidate Database
           {databaseOpen && (
             <div className="database-dropdown">
               <ul>
-                <li ><a href='https://workisy-search-candidate.netlify.app' className='nav-list-item'>Searched Candidates</a></li>
-                <li ><a href='https://saved-condidates.netlify.app/' className='nav-list-item'>Saved Candidates</a></li>
+                <li className='nav-list-item'> Searched Candidates</li>
+                <li className='nav-list-item'>Saved Candidates</li>
                 <li className='nav-list-item'>Unlocked Candidates</li>
               </ul>
             </div>
@@ -46,7 +46,7 @@ const Navbar = () => {
               <li className='nav-list-item'>Usage</li>
               <li className='nav-list-item'>Billing</li>
               <li className='nav-list-item'>Plans & Pricing</li>
-              <li ><a href='https://workisy-login.netlify.app' className='nav-list-item'>Logout</a></li>
+              <li className='nav-list-item'>Logout</li>
             </ul>
           </div>
         )}
